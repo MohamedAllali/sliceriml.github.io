956bb80bd558f4feb906c925e935e254d70f95ea
Mohamed Alalli BILAL <92948337+MohamedAllali@users.noreply.github.com>
2024-10-31 14:10:07 +0000

    Add files via upload

---
+++ old/index.js
@@ -0,0 +1,23 @@
+const bgColors = ['#6a96d9', '#3a9d56', '#db4141', '#fda843', '#e1da2f'];
+// const bgColors = ['#6495ed', '#2e8b57', '#cd5c5c', '#f4a460', '#e1b12f'];
+
+window.onload = function () {
+	const menuItems = document.querySelectorAll('nav li a');
+
+	for (let i = 0; i < bgColors.length; i++) {
+		menuItems[i].style.backgroundColor = bgColors[i];
+		menuItems[i].onclick = function () {
+			// const titre = document.querySelector('main h1:first-child');
+			// titre.innerHTML = this.innerHTML;
+			borderTopLeft.style.borderColor = borderBottomRight.style.borderColor = bgColors[i];
+		}
+		// borderTopLeft.style.height = borderBottomRight.style.height = borderTopLeft.style.width;
+		// borderBottomRight.style.height = borderBottomRight.style.
+	}
+
+	// main.onload = resizeIframe;
+}
+
+function resizeIframe(iFrame) {
+	iFrame.style.height = iFrame.contentWindow.document.documentElement.scrollHeight + 'px';
+}
